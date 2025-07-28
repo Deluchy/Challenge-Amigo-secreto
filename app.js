1b5e9c1366fd3b3dbc13eb533c9300b7bb606f42
Deluchy <deyvi.matos.c@gmail.com>
2025-07-28 17:08:06 -0400

    Escribiendo la logica del app.js
    definiendo la lista de amigos seguida de funcion  para agregar un amigo a la lista.
    
    usando const para añadirAmigo y nombreAmigo.
     const añadirAmigo = document.getElementById('amigo');
        const nombreAmigo = añadirAmigo.value.trim();

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigos = [];
+
+// Función para agregar un amigo a la lista
+function agregarAmigo() {
+    const añadirAmigo = document.getElementById('amigo');
+    const nombreAmigo = añadirAmigo.value.trim();
+
+}
