f18b2e7d8910971b8556a7b2f3bd0c70751ae510
Deluchy <deyvi.matos.c@gmail.com>
2025-07-28 21:05:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+//Array para almacenar los nombres.
 let listaAmigos = [];
 
 // Función para agregar un amigo a la lista
@@ -8,4 +9,25 @@ function agregarAmigo() {
     const añadirAmigo = document.getElementById('amigo');
     const nombreAmigo = añadirAmigo.value.trim();
 
+    // Valida que el campo escriba un nombre no esté vacío o contenga espacio en blanco.
+    if (nombreAmigo === '') {
+        alert('Por favor, no dejar el espacio en blanco');
+        return;
+    }
+
+    // Evitar Nombres duplicados
+    if (listaAmigos.includes(nombreAmigo)) {
+        alert('Este nombre ya está en la lista, por favor ingrese otro nombre');
+        return;
+    }
+
+    // Agregar el nombre al listaAmigos.
+    listaAmigos.push(nombreAmigo);
+    
+    // Limpiar el añadirAmigo luego de presionar el botón Añadir.
+    añadirAmigo.value = '';
+    
+    // Actualizar la lista visual para mostrar los amigos agregados.
+    actualizarListaAmigos();
 }
+
